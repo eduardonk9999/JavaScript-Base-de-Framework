7b334b79e9bc5c200bd91b2ee5549dfca9e95b2c
Eduardo <thiago.novak7@gmail.com>
2020-09-15 22:01:43 -0300

    Add, api

---
+++ API/HTTP/app.js
@@ -0,0 +1,27 @@
+/*
+ API
+
+ Application
+ - Um servidor, aplicativo, objeto JS ou qualquer outra coisa que você
+ interaja através de comandos. Ao digitar uma URL, estamos utilizando
+ a API do browser para se comunicar com a API do servidor.   
+
+ Programming
+ - Programação, isso siguinifica que um comando irá encadear uma cadeia de eventos
+ pré-definidos. O resultado esperado é geralmente o mesmo.
+
+
+ Interface
+- A interface são os comandos criados para permirir a interação com a aplicação.
+Ex: 'VIOLAO'.toLowerCase() é um método que faz parte da interface do objeto string.
+A interação com a interface retorna um efeito de resposta.
+ -
+ */
+
+
+ /*
+  CORS
+  Cross-Origin Reseource Sharing, gerencia como deve ser o compartilhamento
+  de recursos entre diferente origens.
+ 
+ */
