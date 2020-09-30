00434818bdc79adea5bf97173522dc2250de27e7
Eduardo <thiago.novak7@gmail.com>
2020-09-29 21:14:05 -0300

    Fetch

--- async-await/anoits.js
+++ async-await/anoits.js
@@ -1,6 +1,10 @@
 /**
  A palavra chave async indica que a função possui partes assíncronas e que você pretende esperar a resolução da mesma antes de continuar.
  O await irá indicar a promise que devemos esperar. Faz parte do ES8
+
+
+ try = é para pegar os erros da requisição.
+ Ele vai pegar qualquer tipo de erro, quando houver erro ele vai impedir que ocorra a requisição.
  */
 
  async function puxarDados() {
