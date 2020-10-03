069af465c7927366364695ccb6aceea1daa15ee5
Eduardo <thiago.novak7@gmail.com>
2020-10-03 13:21:53 -0300

    Add, revisao reduce

---
+++ Explorando-Map-Filter-Reduce/pokes.js
@@ -0,0 +1,27 @@
+const pokemons = [
+    {
+        name: 'Pikachu',
+        type: 'eletric'
+    },
+    {
+        name: 'Squirtle',
+        type: 'water'
+    },
+    {
+        name: 'Magikarp',
+        type: 'water'
+    }
+];
+
+const pokemonsPorTipo = pokemons.reduce((pokemonsPorTipo, pokemonAtual) => {
+    console.log(pokemonAtual);
+
+    pokemonsPorTipo[pokemonAtual.type] = pokemonsPorTipo[pokemonAtual.type] || [];
+    pokemonsPorTipo[pokemonAtual.type].push(pokemonAtual);
+
+    return pokemonsPorTipo;
+
+}, {})
+
+
+console.log(pokemonsPorTipo.water)
---
+++ Explorando-Map-Filter-Reduce/reduce.js
@@ -0,0 +1,27 @@
+const numeros = [10, 20,30,40,50];
+
+const total = numeros.reduce((acc, item) => {
+
+    return acc + item;
+});
+
+/*
+Ele sempre vai retornar um valor final, toda vez que vc precisar pegar varios itens de um array e transformar em uma valor só,
+vamos usar o reduce
+
+*/
+
+
+// String
+const productList = cart.reduce((acc, { name }) => {
+
+}, '');
+
+
+
+// Objeto
+const productList = cart.reduce((acc, { name }) => {
+
+}, {});
+
+
